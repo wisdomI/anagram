c9f7f23eaf6a05652d5d3ba446a11d5aafc93bbb
wisdomI <81252474+wisdomI@users.noreply.github.com>
2022-05-28 16:08:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def find_anagram(w1, w2):
+    if(len(w1) == len(w2)):
+        sorted_w1=sorted(w1)
+        sorted_w2=sorted(w2)
+
+        if (sorted_w1 == sorted_w2):
+                print("True")
+        else: print("False")
+
+    else: 
+        print("False")
+
+
+w1 = input("input text here ")
+w2 = input("input text here ")
+
+find_anagram(w1, w2)
